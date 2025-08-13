8aebfa09bcbfc2550028fd6de348ae16da656932
nancygarg27 <ngarg7844@gmail.com>
2025-08-13 21:05:53 +0530

    Add files via upload

---
+++ q1partaANDb.cpp
@@ -0,0 +1,12 @@
+// Develop a Menu driven program to demonstrate the following operations of Arrays
+// PART A: CREATE AN ARRAY and PART B: DISPLAY AN ARRAY
+ # include <iostream>
+ using namespace std;
+ int main (){
+ int arr[]= {1,2,3,4,5};
+ int n= sizeof(arr)/ sizeof(int);
+ for(int i=0; i<n; i++){
+ cout<<arr[i]<<" ";
+ }
+ return 0;
+ }
